a38a5e979c04bc60cb37021e9b3aaeff956435a4
anselme <azur@gmail.com>
2026-08-26 15:49:55 +0100

    Add the authentification fonction firstly

---
+++ auth/auth.py
@@ -0,0 +1,22 @@
+from passlib.context import CryptContext
+from jose import jwt, JWTError
+
+ALGORITHM = "HS256"
+KEY = "MX565"  # Replace with your actual secret key
+ACCESS_TOKEN_EXPIRE_MINUTES = 30  # Token expiration time in minutes
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+# Hash the password using bcrypt
+def hash_password(password: str):
+    return pwd_context.hash(password)
+
+# Verify the password against the hashed password
+def password_verify(plain_password: str, hashed_password:str):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes= ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, KEY, algorithm= ALGORITHM)
---
+++ database/database.py
@@ -0,0 +1,7 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+# Initialize the database engine and session
+engine = create_engine("mysql://root:@localhost/mydatabase")
+Base = declarative_base()
+Session = sessionmaker(bind=engine)
